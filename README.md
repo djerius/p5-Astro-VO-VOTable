35188107332928410d8d2cde1228faed9d3098c8
Diab Jerius <djerius@cfa.harvard.edu>
2022-07-19 13:57:28 -0400

    move README contents into Astro::VO::VOTable, generate README.md on build

---
+++ README.md
@@ -0,0 +1,86 @@
+# NAME
+
+Astro::VO::VOTable - VOTable document manipulation package
+
+# VERSION
+
+version 1.1
+
+# SYNOPSIS
+
+use Astro::VO::VOTable;
+
+# DESCRIPTION
+
+The VOTABLE package is a set of Perl modules which provide an
+object-oriented interface to VOTABLE-formatted XML files. The software
+is built atop the Perl [XML::LibXML](https://metacpan.org/pod/XML%3A%3ALibXML) module, which is a Perl interface
+to the C-based libxml2 XML parser.
+
+The VOTABLE software provides a class for each valid element defined
+in the VOTABLE DTD.
+
+The programs in the 'examples' subdirectory illustrate the use of the
+modules to read a VOTABLE document and access its parts, and the
+construction of a VOTABLE document from scratch, and printing it out.
+
+## Outline
+
+Each VOTABLE element has a class for it. All of the element classes
+inherit from the [Astro::VO::VOTable::Element](https://metacpan.org/pod/Astro%3A%3AVO%3A%3AVOTable%3A%3AElement) class, which itself inherits from
+the [XML::LibXML::Element](https://metacpan.org/pod/XML%3A%3ALibXML%3A%3AElement) class. Therefore, if you need functionality
+that is not provided by the VOTable methods, you can probably get the
+needed functionality by calling the [XML::LibXML::Element](https://metacpan.org/pod/XML%3A%3ALibXML%3A%3AElement) methods. In a similar vein,
+the [AO::VOTable::Document](https://metacpan.org/pod/AO%3A%3AVOTable%3A%3ADocument) class inherits from [XML::LibXML::Document](https://metacpan.org/pod/XML%3A%3ALibXML%3A%3ADocument)q.
+
+Each element class provides get, set, remove, and append accessors for
+each child element, and get, set, and remove accessors for each
+attribute. The code throws an exception if you use an invalid child
+element or invalid attribute.
+
+The set and append methods for child elements all take list
+arguments. For elements with individual child elements, the list
+should contain a single element; the code does NOT enforce this
+restriction at this point, so be careful.
+
+The get methods for child elements all return lists, even when only
+one element is expected. Therefore, the get accessor results should be
+indexed to get the desired element.
+
+All get, set, append, and remove methods are implemented by the
+AUTOLOAD method in [Astro::VO::Table::Element](https://metacpan.org/pod/Astro%3A%3AVO%3A%3ATable%3A%3AElement), greatly decreasing the amount of code
+needed to support new elements.
+
+# INTERNALS
+
+# SEE ALSO
+
+XML::LibXML
+
+# SUPPORT
+
+## Bugs
+
+Please report any bugs or feature requests to bug-astro-vo-votable@rt.cpan.org  or through the web interface at: https://rt.cpan.org/Public/Dist/Display.html?Name=Astro-VO-VOTable
+
+## Source
+
+Source is available at
+
+    https://github.com/djerius/p5-Astro-VO-VOTable
+
+and may be cloned from
+
+    https://github.com/djerius/p5-Astro-VO-VOTable.git
+
+# AUTHORS
+
+- Diab Jerius <djerius@cpan.org>
+- Eric L Winter
+
+# COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2022 by Smithsonian Astrophysical Observatory; 2002 Laboratory for High-Energy Astrophysics, NASA GSFC.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
--- lib/Astro/VO/VOTable.pm
+++ lib/Astro/VO/VOTable.pm
@@ -1,17 +1,18 @@
-# VOTable.pm
+package Astro::VO::VOTable;
 
-# $Id: VOTable.pm,v 1.1.1.2 2003/11/13 21:31:24 elwinter Exp $
+# ABSTRACT: VOTable document manipulation package
 
-# NOTE: All internal subroutine names start with a leading underscore
-# (_) character, and assume that their inputs are valid.
+use strict;
+use diagnostics;
+use warnings;
 
-#******************************************************************************
+our $VERSION = '1.1';
 
-=pod
+# COPYRIGHT
 
-=head1 NAME
+1;
 
-Astro::VO::VOTable - VOTable document manipulation package
+__END__
 
 =head1 SYNOPSIS
 
@@ -19,97 +20,45 @@ use Astro::VO::VOTable;
 
 =head1 DESCRIPTION
 
-This code can be used to manipulate VOTable-format XML documents. See
-the subsidiary module documentation for details.
+The VOTABLE package is a set of Perl modules which provide an
+object-oriented interface to VOTABLE-formatted XML files. The software
+is built atop the Perl L<XML::LibXML> module, which is a Perl interface
+to the C-based libxml2 XML parser.
 
-=head1 WARNINGS
+The VOTABLE software provides a class for each valid element defined
+in the VOTABLE DTD.
 
-=over 4
+The programs in the 'examples' subdirectory illustrate the use of the
+modules to read a VOTABLE document and access its parts, and the
+construction of a VOTABLE document from scratch, and printing it out.
 
-=item
+=head2 Outline
 
-None.
+Each VOTABLE element has a class for it. All of the element classes
+inherit from the L<Astro::VO::VOTable::Element> class, which itself inherits from
+the L<XML::LibXML::Element> class. Therefore, if you need functionality
+that is not provided by the VOTable methods, you can probably get the
+needed functionality by calling the L<XML::LibXML::Element> methods. In a similar vein,
+the L<AO::VOTable::Document> class inherits from L<XML::LibXML::Document>q.
 
-=back
+Each element class provides get, set, remove, and append accessors for
+each child element, and get, set, and remove accessors for each
+attribute. The code throws an exception if you use an invalid child
+element or invalid attribute.
+
+The set and append methods for child elements all take list
+arguments. For elements with individual child elements, the list
+should contain a single element; the code does NOT enforce this
+restriction at this point, so be careful.
+
+The get methods for child elements all return lists, even when only
+one element is expected. Therefore, the get accessor results should be
+indexed to get the desired element.
+
+All get, set, append, and remove methods are implemented by the
+AUTOLOAD method in L<Astro::VO::Table::Element>, greatly decreasing the amount of code
+needed to support new elements.
 
 =head1 SEE ALSO
 
-=over 4
-
-=item
-
 XML::LibXML
-
-=back
-
-=head1 AUTHOR
-
-Eric Winter, NASA GSFC (Eric.L.Winter.1@gsfc.nasa.gov)
-
-=head1 VERSION
-
-$Id: VOTable.pm,v 1.1.1.2 2003/11/13 21:31:24 elwinter Exp $
-
-=cut
-
-#******************************************************************************
-
-# Revision history
-
-# $Log: VOTable.pm,v $
-# Revision 1.1.1.2  2003/11/13 21:31:24  elwinter
-# Changed name to Astro::VO::VOTable.
-#
-# Revision 1.1.1.1  2002/10/25 18:30:48  elwinter
-# Changed required Perl version to 5.6.0.
-#
-# Revision 1.1  2002/09/06  19:28:31  elwinter
-# Initial revision
-#
-
-#******************************************************************************
-
-# Begin the package definition.
-package Astro::VO::VOTable;
-
-#******************************************************************************
-
-# Compiler pragmas.
-use strict;
-use diagnostics;
-use warnings;
-
-#******************************************************************************
-
-# Module version.
-our $VERSION = '1.1';
-
-#******************************************************************************
-
-# Specify external modules to use.
-
-# Standard modules.
-
-# Third-party modules.
-
-# Project modules.
-
-#******************************************************************************
-
-# Class constants.
-
-#******************************************************************************
-
-# Class variables.
-
-#******************************************************************************
-
-# Local subroutines.
-
-#******************************************************************************
-
-# Method definitions
-
-#******************************************************************************
-1;
-__END__
